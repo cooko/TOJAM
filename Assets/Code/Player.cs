ffddee2feecf9f3608951f8af8fa51c81b9c18b7
Oliver Cook <cooko@uoguelph.ca>
2013-05-04 11:41:54 -0400

    YEAAAAAAHHHHHHH
    lol collision, sorta

--- Assets/Code/Player.cs
+++ Assets/Code/Player.cs
@@ -2,12 +2,16 @@ using UnityEngine;
 using System.Collections;
 
 public class Player : MonoBehaviour {
-
-    public float speed = 6.0F;
-    public float jumpSpeed = 8.0F;
-    public float gravity = 20.0F;
-    private Vector3 moveDirection = Vector3.zero;
 	
+	protected Vector3 move = Vector3.zero;
+	
+	public float moveSpeed = 3f;
+	public float jumpSpeed = 5f;
+	protected bool jump;
+	
+	protected Vector3 gravity = Vector3.zero;
+
+
 	// Use this for initialization
 	void Start () {
 		
@@ -15,37 +19,8 @@ public class Player : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		MoveMe ();
+		if (Input.GetButtonDown("left")){
+			transform.Translate (Vector3.left * 10 * Time.deltaTime);
+		}
 	}
-	
-	void MoveMe () {
-		CharacterController controller = GetComponent<CharacterController>();
-		if (controller.isGrounded) {
-            // We are grounded, so recalculate
-            // move direction directly from axes
-            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0,
-                                    Input.GetAxis("Vertical"));
-            moveDirection = transform.TransformDirection(moveDirection);
-            moveDirection *= speed;
-            
-            if (Input.GetButton ("Jump")) {
-                moveDirection.y = jumpSpeed;
-            }
-        }
-        // Apply gravity
-        moveDirection.y -= gravity * Time.deltaTime;
-        
-        // Move the controller
-        controller.Move(moveDirection * Time.deltaTime);
-	}
-	
-	/*void OnTriggerEnter(Collider other){
-		Debug.Log("PLAYER | Enter");
-		collided = true;
-	}
-	void OnTriggerExit(Collider other){
-		Debug.Log("PLAYER | Exit");
-		collided = false;
-	}*/
-
 }
