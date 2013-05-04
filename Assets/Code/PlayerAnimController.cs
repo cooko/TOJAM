8a694ad61cfb87fc19d9d79aaeffcdc19fe893d6
Oliver Cook <cooko@uoguelph.ca>
2013-05-04 14:34:21 -0400

    Player Animation
    its the animation station

--- Assets/Code/PlayerAnimController.cs
+++ Assets/Code/PlayerAnimController.cs
@@ -4,21 +4,35 @@ using System.Collections;
 public class PlayerAnimController : MonoBehaviour {
 	
 	private bool walking = false;
+	private int facing;
 	private tk2dAnimatedSprite anim;
 	
 	// Use this for initialization
 	void Start () {
+		facing = 1;
 		anim = GetComponent<tk2dAnimatedSprite>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetAxis ("Horizontal") > 0 && !anim.IsPlaying("RunRight")) {
-			Debug.Log ("WALK");
-			anim.Play("RunRight");
+		if (Input.GetAxis ("Horizontal") > 0) {
+			if (facing == 0){
+				anim.FlipX();
+				facing = 1;
+			}
+			if(!anim.IsPlaying("Run"))
+				anim.Play("Run");
 		}
 		else if (Input.GetAxis ("Horizontal") < 0) {
-		
+			if (facing == 1){
+				anim.FlipX ();
+				facing = 0;
+			}
+			if(!anim.IsPlaying("Run"))
+				anim.Play("Run");
+		}
+		else if (Input.GetAxis ("Horizontal") == 0 && !anim.IsPlaying ("Idle")){
+			anim.Play ("Idle");
 		}
 		
 	}
